71658267e8e0f7d760cc65370b3b4e56480aefbc
Alex Fernandez <alex.fernandezdavila.m@gmail.com>
2026-06-15 21:54:58 -0500

    config: update GAS deployment URL (container-bound script)

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,1 +1,1 @@
-export const API_URL = 'https://script.google.com/macros/s/AKfycbyWXmF4sQx_bYjq43ld48aXH4hJe0Ru6uBGNYqW_Q_dDP4af3MMLIf2TpolHfHR2rOW4Q/exec';
+export const API_URL = 'https://script.google.com/macros/s/AKfycby9SHfz8fghQriKEIjqkiocnIO3DC-fJugxI__T0HjfnFhNPbboXQnKdQlwBRpI0ink/exec';
