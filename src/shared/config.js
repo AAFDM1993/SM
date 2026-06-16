9307f063758506eafa467c4d86ae3ec7057ff800
Alex Fernandez <alex.fernandezdavila.m@gmail.com>
2026-06-15 21:48:55 -0500

    config: set production GAS deployment URL

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,1 +1,1 @@
-export const API_URL = 'PENDIENTE_DESPLEGAR';
+export const API_URL = 'https://script.google.com/macros/s/AKfycbyWXmF4sQx_bYjq43ld48aXH4hJe0Ru6uBGNYqW_Q_dDP4af3MMLIf2TpolHfHR2rOW4Q/exec';
